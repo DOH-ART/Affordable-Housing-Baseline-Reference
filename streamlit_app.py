f7d3380a008b2d934fdd3d19d3f26496b2ade922
b-nmadalon <88390918+b-nmadalon@users.noreply.github.com>
2023-03-09 15:04:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,22 +23,27 @@ with datasets:
     st.write(acs_data.head())
     income_data = load_data(income_data_url)
     st.write(income_data.head())
-    acs_baseline_options = load_data(acs_baseline_options_url)
-    st.write(acs_baseline_options.head())
-    HVtoIncome_slider = st.slider('Home Value to Income Ratio',2.5,4.5)
-    
+    HVtoIncome_slider = st.slider('Home Value to Income Ratio',2.5,4.5,3.5,.01)
+
+acs_data['geography_name'] = acs_data['geography_name'].astype(str)
+
 col1, col2 = st.columns((1,1))
 with col1:
-    SaleUnitAvailabilityRate = st.slider('Sale unit Availability Rate',0,100)
+    SaleUnitAvailabilityRate = st.slider('Sale unit Availability Rate',0,100,100)
 with col2:
-    RentalUnitAvailabilityRate = st.slider('Rental Unit Availability Rate',0,100)
+    RentalUnitAvailabilityRate = st.slider('Rental Unit Availability Rate',0,100,60)
 
 #Input widgets for sidebar
 with st.sidebar:
-    location = st.selectbox('County or Municipality',list(set(acs_baseline_options['locality name'].tolist())))
-    adjacency = st.selectbox('County or Municipality Adjacency',acs_baseline_options['income limit type'][acs_baseline_options['locality name'] == location].tolist())
-    HH_size = st.slider('Household Size',1,5)
-    ILY = st.selectbox('Income Limit Year',['2015','2016','2017'])
-    IL = st.selectbox('Income Limit',['limit 1','limit 2'])
+    location = st.selectbox('County or Municipality',list(set(acs_data['geography_name'][~acs_data['geography_name'].str.contains('nan')].tolist())))
+    geoid_location = list(set(acs_data['geoid'][acs_data['geography_name'] == location]))
+    adjacency = st.selectbox('County or Municipality Adjacency',list(set(income_data['il_type'][income_data['geoid'] == geoid_location[0]].tolist())))
+    HH_size = st.slider('Household Size',1,5,3)
+    ILY = st.selectbox('Income Limit Year',list(set((income_data['il_year']))))
+    IL = st.selectbox('Income Limit',list(set(income_data['income_limit'][income_data['geoid'] == geoid_location[0]].tolist())))
 
 
+locality_submitted = acs_data[acs_data['geoid'] == geoid_location[0]]
+LocalitySubmittedSum = sum(locality_submitted['estimate'])*HVtoIncome_slider
+result = LocalitySubmittedSum * 3.333 * 12
+st.metric(label = 'Baseline Estimate', value = result)
